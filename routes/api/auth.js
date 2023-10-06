7f0cedfe3100b29af2be769948225ce1f8978533
Basi10 <basilelbirru@gmail.com>
2023-10-06 20:09:34 +0300

    added custom auth middleware & JWT Verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
